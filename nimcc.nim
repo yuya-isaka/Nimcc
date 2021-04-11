ebcc59ed6f388b11952d2b1496964c8ed15cb403
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-04-11 16:48:02 +0900

    nimcc 改良，入力文字列を表示してインデックスを一つ進める関数用意

--- nimcc.nim
+++ nimcc.nim
@@ -3,6 +3,10 @@ import os
 import system
 import strformat
 
+proc convertInput(input: seq, index: var int): string =
+  result = input[index]
+  inc(index)
+
 if os.paramCount() >= 2:
   quit("引数の個数が正しくありません．")
 
@@ -12,23 +16,20 @@ let input = os.commandLineParams()
 echo ".intel_syntax noprefix"
 echo ".global _main"
 echo "_main:"
-echo strformat.fmt"  mov rax, {input[i]}"
-inc(i)
+echo strformat.fmt"  mov rax, {convertInput(input, i)}"
 
 while len(input) >= i:
   if input[i] == "+":
     inc(i)
-    echo strformat.fmt"  add rax, {input[i]}"
-    inc(i)
+    echo strformat.fmt"  add rax, {convertInput(input, i)}"
     continue
 
   if input[i] == "-":
     inc(i)
-    echo strformat.fmt"  sub rax, {input[i]}"
-    inc(i)
+    echo strformat.fmt"  sub rax, {convertInput(input, i)}"
     continue
 
-  quit(strformat.fmt"予期しない文字です．:{input[i]}")
+  system.quit(strformat.fmt"予期しない文字です．:{input[i]}")
 
 echo "  ret"
 system.quit(0)

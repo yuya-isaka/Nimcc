452c060fbee2b032e0e21d3470ea9dd99b57cb13
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-04-23 21:05:48 +0900

    Add codegen comment

--- codegen.nim
+++ codegen.nim
@@ -14,15 +14,14 @@ proc gen(node: Node)
 proc genAddr(node: Node) =
   case node.kind:
   of NdLvar:
-    echo fmt"  lea rax, [rbp-{node.arg.offset}]"  #! アドレス計算を行うが，メモリアクセスは行わず，アドレス計算の結果そのものをraxに代入
-    #! raxにはアドレスが入ってる
-    echo "  push rax"
+    echo fmt"  lea rax, [rbp-{node.arg.offset}]"  #! アドレス計算を行うが，メモリアクセスは行わず，アドレス計算の結果そのもの(アドレス）をraxに代入
+    echo "  push rax"                             #! raxにはアドレスが入ってる!!!重要だよ！！（評価結果じゃないんだよ！）
     return
   of NdDeref:
-    gen(node.lhs) #! *p = 3 のようにデリファレンス経由で値を代入するときに対応するため， pのアドレスが生成されるように左辺値をコンパイル
+    gen(node.lhs)                                 #! *p = 3 のようにデリファレンス経由で値を代入するときに対応するため， pのアドレスが生成されるように左辺値をコンパイル
     return
   else:
-    errorAt("not an lvalue", node.tok)  #! Token型を渡す設計にすることで， コードジェネレートの際のエラー位置を正確に確認できるようになった（本当か
+    errorAt("not an lvalue", node.tok)            #! Token型を渡す設計にすることで， コードジェネレートの際のエラー位置を正確に確認できるようになった（本当か
 
 # 関数フレーム，プロローグ
 proc load() =
@@ -35,13 +34,13 @@ proc store() =
   echo "  pop rdi"
   echo "  pop rax"
   echo "  mov [rax], rdi"
-  echo "  push rdi"     #! NdAssignはNdExprStmtにラップされてるから，ここでpushしたものは，add rsp, 8で抜き取られる(ちゃんと取り除ける)
+  echo "  push rdi"                 #! NdAssignはNdExprStmtにラップされてるから，ここでpushしたものは，add rsp, 8で抜き取られる(ちゃんと取り除ける)
 
 #--------------------------------------------------------
 
-var labelSeq: int #! 0で初期化してくれる
+var labelSeq: int                                     #! 0で初期化してくれる
 var funcname: string
-#! 引数は6つ
+                                                      #! 引数は6つ
 var argreg = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"] #! 関数の引数の順番． x86-64, ABI仕様で決められている．　このルールに従わないと適切な機械語を生成できない
 
 #-----------------------------------------------------
@@ -49,33 +48,32 @@ var argreg = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"] #! 関数の引数の順�
 # コードジェネレート
 proc gen(node: Node) =
 
-  #! ここはreturnされるcase switch文
-  case node.kind
+  case node.kind                    #! ここはreturnされるcase switch文
   of NdNum:
     echo fmt"  push {node.val}"
-    return  #! 計算しないNodeKindは全てリターン
+    return                          #! 計算しないNodeKindは全てリターン
   of NdExprStmt:
     gen(node.lhs)
-    echo "  add rsp, 8"   #! pop raxをする代わり？ スタックポインタを上に8あげればpopしたのと同じ.
+    echo "  add rsp, 8"             #! pop raxをする代わり？ スタックポインタを上に8あげればpopしたのと同じ.
     return
-  of NdLvar:
-    genAddr(node)
-    load()
+  of NdLvar:                        #todo 変数利用
+    genAddr(node)                   #! 変数を右辺値として扱う場合は， まず左辺値として評価
+    load()                          #! スタックトップにある結果をアドレスとみなして，そのアドレスから値をロード
     return
-  of NdAssign:
-    genAddr(node.lhs)
-    gen(node.rhs)
-    store()
+  of NdAssign:                      #todo 代入
+    genAddr(node.lhs)               #! 左辺値からアドレス生成
+    gen(node.rhs)                   #! 右辺値としてコンパイルして結果を生成
+    store()                         #! スタックトップに値があるから，それを左辺値のアドレスに代入
     return
-  of NdAddr:
-    genAddr(node.lhs) #! 左辺値としてコンパイル！！！！！！！！！！！！！！！ -> アドレスを計算してスタックに格納
+  of NdAddr:                        #todo アドレス生成
+    genAddr(node.lhs)               #! 左辺値としてコンパイル！！！！！！！！！！！！！！！ -> アドレスを計算してスタックに格納
     return
-  of NdDeref:
-    gen(node.lhs) #! NdLvarかNdAddrかNdDerefなどの可能性があるため**a， genで右辺値としてコンパイル！！！ -> どこかでgenAddrが生成される
-    load()
+  of NdDeref:                       #todo デリファレンス
+    gen(node.lhs)                   #! 右辺値としてコンパイル. ->　何らかのアドレスを計算するコードに変換されるはず(そうでなければその結果をデリファレンスすることはできない．その場合はエラーにする） -> 最終的にgenAddrをどこかで呼び出すということ
+    load()                          #! 何らかのアドレスを計算した後， スタックに評価結果を残す， それをロード
     return
-  of NdIf:  #todo if文はこのNodeKind
-    var seq = labelSeq  #! ラベル番号はユニークにする
+  of NdIf:
+    var seq = labelSeq              #! ラベル番号はユニークにする
     inc(labelSeq)
     if node.els != nil:
       gen(node.cond)
@@ -84,7 +82,7 @@ proc gen(node: Node) =
       echo fmt"  je .Lelse{seq}"
       gen(node.then)
       echo fmt"  jmp .Lend{seq}"
-      echo fmt".Lelse{seq}:"  #! :を付け忘れた覚書
+      echo fmt".Lelse{seq}:"        #! :を付け忘れた覚書
       gen(node.els)
       echo fmt".Lend{seq}:"
     else:
@@ -95,7 +93,7 @@ proc gen(node: Node) =
       gen(node.then)
       echo fmt".Lend{seq}:"
     return
-  of NdWhile: #todo while文はこのNodeKind
+  of NdWhile:
     var seq = labelSeq
     inc(labelSeq)
     echo fmt".Lbegin{seq}:"
@@ -103,11 +101,11 @@ proc gen(node: Node) =
     echo "  pop rax"
     echo "  cmp rax, 0"
     echo fmt"   je .Lend{seq}"
-    gen(node.then)
+    gen(node.then)                  #! node.thenはExprStmt()にラップされてるから，スタックトップに値は残らない
     echo fmt"   jmp .Lbegin{seq}"
     echo fmt".Lend{seq}:"
-    return    #! return忘れてた覚書
-  of NdFor: #todo for文はこのNodeKind
+    return                          #! return忘れてた覚書
+  of NdFor:
     var seq = labelSeq
     inc(labelSeq)
     if node.init != nil:
@@ -124,61 +122,60 @@ proc gen(node: Node) =
     echo fmt"   jmp .Lbegin{seq}"
     echo fmt".Lend{seq}:"
     return
-  of NdBlock: #todo {}の中身はひたすら生成, body: seq[Node]
+  of NdBlock:                       #todo {}の中身はひたすら生成, body: seq[Node]
     for tmp in node.body:
-      gen(tmp)
-      # echo "  pop rax" # 多分readExprStmtのおかげでいらなくなった（便利です）
+      gen(tmp)                      #! ExprStmt()にラップされてるから，スタックトップに値は残らない
+                                    # -> echo "  pop rax" # readExprStmtのおかげでいらなくなった（便利です）
     return
-  of NdFuncall: #todo 関数の時はこのNodeKind
-
+  of NdFuncall:
     #* 関数の引数をローカル変数として格納
     var nargs = 0
     var arg = node.args
     while arg != nil:
-      gen(arg)  #! 順番にスタックにpushされている．
+      gen(arg)                      #! 順番にスタックにPUSHされている．
       inc(nargs)
       arg = arg.next
 
     var i = nargs - 1
     while i >= 0:
-      echo fmt"  pop {argreg[i]}" #! 順番にスタックからpopされている．(ABI仕様), rspが関数の開始位置まで戻る
+      echo fmt"  pop {argreg[i]}"   #! 順番にスタックからPOPされている．(ABI仕様), RSPが関数の開始位置(RBP)まで戻る
       dec(i)
 
     var seq = labelSeq
     inc(labelSeq)
-    echo "  mov rax, rsp" #! raxは16の倍数じゃないとダメ！
-    echo "  and rax, 15"  #! 関数を呼ぶ前にRSPを調整するようにして、RSPを16の倍数になるように調整(pushやpopはRSPを8バイト単位で変更するから、call命令を発行するときに必ずしもRSPが16の倍数になっているとは限らん)
-    echo fmt"  jnz .Lcall{seq}" #! 比較結果が0じゃなかったら飛ぶ
+    echo "  mov rax, rsp"           #! 関数呼び出しの前にRSPは16の倍数じゃないとダメ！
+    echo "  and rax, 15"            #! 関数を呼ぶ前にRSPを16の倍数になるように調整(PUSHやPOPはRSPを8バイト単位で変更するから、call命令を発行するときに必ずしもRSPが16の倍数になっているとは限らん)
+                                    #! and 15, 15 -> 15   and 16, 15 -> 0   and 17, 15 -> 1
+    echo fmt"  jnz .Lcall{seq}"     #! 比較結果!=0で飛ぶ(RAXが16の倍数じゃない場合飛ぶ)
     echo "  mov rax, 0" 
     echo fmt"  call {node.funcname}"
     echo fmt"  jmp .Lend{seq}"
-    echo fmt".Lcall{seq}:"
-    echo "  sub rsp, 8" #! jnz .Lcall{seq}が発動されるとここに飛んできて，スタックを伸ばす
-    echo "  mov rax, 0" 
+    echo fmt".Lcall{seq}:"          #! jnz .Lcallで飛んでくる
+    echo "  sub rsp, 8"             #! スタックを伸ばす(RSPが16の倍数になるように調整)
+    echo "  mov rax, 0"             #! RAX初期化 
     echo fmt"  call {node.funcname}"
-    echo "  add rsp, 8" #! 関数読んだ後にスタックを縮ませる
+    echo "  add rsp, 8"             #! スタックを縮ませ元に戻す
     echo fmt".Lend{seq}:"
-    echo "  push rax"
+    echo "  push rax"               #! 評価結果を格納
     return
-  of NdReturn:  #todo returnの時は左辺を生成してpopreturn
+  of NdReturn:
     gen(node.lhs)
-    echo "  pop rax"    #! これまでは毎回ポップしていたが，returnの時だけポップするので良い(複数のノードを生成しない)
+    echo "  pop rax"                #! 関数全体の評価結果がスタックトップ(RSP)に入っている．　取り出してRAXにセット．
     echo fmt"  jmp .Lreturn.{funcname}"
     return
   else:
-    discard #! ここで捨てないと下の処理見れない
+    discard                         #! ここで捨てないと下の処理見れない
 
-  gen(node.lhs)   #! これより下のNode型は，2つの値を使用する計算だから，まとめて上でgen(node.lhs),gen(node.rhs)している
-  gen(node.rhs)   #! case文の各Node型の中で実行しても良い
+  gen(node.lhs)                     #! これより下のNode型は，2つの値を使用する計算だから，まとめて上でgen(node.lhs),gen(node.rhs)している
+  gen(node.rhs)
 
   echo "  pop rdi"
   echo "  pop rax"
 
-  #todo 計算&比較ふぇーーーーーーーーーず(returnせず，スタックに値を保存するだけ)
-  case node.kind
+  case node.kind                    #todo 計算&比較ふぇーーーーーーーーーず(returnせず，スタックに値を保存するだけ)
   of NdAdd:
     if node.ty.kind == TyPtr:
-      echo "  imul rdi, 8"    # rdi = rdi * 8
+      echo "  imul rdi, 8"          #! rdi = rdi * 8  ローカル変数は8バイトずつ並べてるから，ポインタの時，+1は+8と設定
     echo "  add rax, rdi"
   of NdSub:
     if node.ty.kind == TyPtr:
@@ -208,18 +205,16 @@ proc gen(node: Node) =
   else:
     discard
 
-  echo "  push rax"   #! 式全体の結果を，スタックトップにプッシュ
+  echo "  push rax"                  #! 式全体の結果を，スタックトップにプッシュ
 
 #--------------------------------------------------------------------------
 
-# 完成形アセンブリ出力関数
-proc codegen*(prog: Function) =
-  # 始まり
-  echo ".intel_syntax noprefix"
+proc codegen*(prog: Function) =       # 完成形アセンブリ出力関数
+  echo ".intel_syntax noprefix"       # 始まり
 
   var fn = prog
   while fn != nil:
-    echo fmt".global {fn.name}" # macだと_mainで動く
+    echo fmt".global {fn.name}"       # macだと_mainで動く
     echo fmt"{fn.name}:"
     funcname = fn.name
 
@@ -229,25 +224,24 @@ proc codegen*(prog: Function) =
     echo fmt"  sub rsp, {fn.stackSize}"
 
     var i = 0
-    #! vlの中身は,0-6の連結
-    var vl = fn.params  #! ここでオフセットを指定するためにparamsは使われる．ここには引数だけ入ってる
+                                      #! vlの中身は,0-6の連結
+    var vl = fn.params                #! ここでオフセットを指定するためにparamsは使われる．ここには引数だけ入ってる
     while vl != nil:
       var lvar = vl.lvar
-      #!  レジスタの値をそのローカル変数のためのスタック上の領域に書き出し(ローカル変数と同じように扱える)
-      echo fmt"  mov [rbp-{lvar.offset}], {argreg[i]}" #! 最初に6つのローカル変数のぶんのスタック領域を確保しておく（空白で良い）
+                                                        #!  レジスタの値をそのローカル変数のためのスタック上の領域に書き出し(ローカル変数と同じように扱える)
+      echo fmt"  mov [rbp-{lvar.offset}], {argreg[i]}"  #! 最初に6つのローカル変数のぶんのスタック領域を確保しておく（空白で良い）
       inc(i)
       vl = vl.next
 
-    # プログラムに入ってるノードが尽きるまでアセンブリ生成(連結リストだからこの書き方ができる)
-    var node = fn.node
+    var node = fn.node                # プログラムに入ってるノードが尽きるまでアセンブリ生成(連結リストだからこの書き方ができる)
     while node != nil:
       gen(node)
       node = node.next
 
     # *エピローグ
     echo fmt".Lreturn.{funcname}:"
-    echo "  mov rsp, rbp"
-    echo "  pop rbp"
-    echo "  ret"
+    echo "  mov rsp, rbp"             #! RSP(スタックトップ)をRBPの位置に移動
+    echo "  pop rbp"                  #! POPするとRSP兼RBPの値(上にあるRBPのアドレス)をスタックから取り出して， RBPにストア -> RBPが元に戻る(上のRBPに戻る) -> RSPがリターンアドレスを指す
+    echo "  ret"                      #! スタックからアドレスを一つポップ(リターンアドレスのはず），　そのアドレスにジャンプ
 
     fn = fn.next

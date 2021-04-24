2dcd28d5bddb69fccd76ec66af76545711e13054
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-04-24 16:04:36 +0900

    Add type Int

--- header.nim
+++ header.nim
@@ -1,6 +1,6 @@
 
 #[
-  * enum, object, グローバル変数, 入力文字列, などを管理
+  * enum, object, グローバル変数, 入力文字列, ライブラリ関数(errorAt) などを管理
 ]#
 
 import os
@@ -14,59 +14,29 @@ if paramCount() != 1:
 for i in commandLineParams()[0]:
   input.add(i)
 
-#--------------------------------------------------------
+#? ------------------------------------------------------------------------------------
 
 # トークンの種類
 type
   TokenKind* = enum
-    TkReserved,   # 記号
-    TkIdent,      # 識別子（変数）
-    TkNum,        # 整数トークン
-    TkEof         # 入力の終わりを表すトークン
+    TkReserved,           # 記号
+    TkIdent,              # 識別子（変数）
+    TkNum,                # 整数トークン
+    TkEof                 # 入力の終わりを表すトークン
 
 # トークン型
 type
   Token* = ref object
-    kind*: TokenKind   # トークンの種類
-    next*: Token       # 次の入力トークン
-    val*: int          # kindがTkNumの場合，その数値
-    str*: string       # トークン文字列
-    at*: int           # 入力文字配列のうち，どこを指しているか（先頭インデックス）
+    kind*: TokenKind      # トークンの種類
+    next*: Token          # 次の入力トークン
+    val*: int             # kindがTkNumの場合，その数値
+    str*: string          # トークン文字列
+    at*: int              # 入力文字配列のうち，どこを指しているか（先頭インデックス）
 
-#! 現在着目しているトークン
+# 現在着目しているトークン
 var token*: Token = nil
 
-#----------------------------------------------------------
-
-# ローカル変数の型(連結リスト)
-type
-  Lvar* = ref object
-    name*: string
-    offset*: int
-
-type
-  LvarList* = ref object
-    next*: LvarList
-    lvar*: Lvar
-
-#-----------------------------------------------------------
-
-# エラー表示関数(今微妙に使い勝手が悪い．ので後々書き換える)
-# メッセージとトークンを受け取って，そのトークンの位置に値を挿入する
-proc errorAt*(errorMsg: string, tok: Token) = 
-  var tmp: string
-  for i in input:
-    tmp.add($i)
-  echo tmp
-  # nilを引数で渡せる？
-  if tok == nil: # 初期化されていない参照型Objectはnilとなる # 例外チェック # 本来はoption型とかを使うべき
-    echo " ".repeat(idx) & "^"
-  else:
-    echo " ".repeat(tok.at) & "^"
-  # echo idx
-  quit(errorMsg)
-
-# ------------------------------------------------------------------------------------
+#? ------------------------------------------------------------------------------------
 
 # 型の種類
 type
@@ -74,52 +44,83 @@ type
     TyInt,
     TyPtr
 
-# 型型
+# Type型
 type
   Type* = ref object
-    kind*: TypeKind
-    base*: Type   # TyPtrの時, 対象変数
+    kind*: TypeKind       # 型の種類
+    base*: Type           # TyPtrの時, 対象変数
 
-# ------------------------------------------------------------------------------------
+#? ------------------------------------------------------------------------------------
+
+# ローカル変数の型
+type
+  Lvar* = ref object
+    name*: string
+    offset*: int
+    ty*: Type
+
+# ローカル変数の連結リスト
+type
+  LvarList* = ref object
+    next*: LvarList
+    lvar*: Lvar
+
+#? ------------------------------------------------------------------------------------
+
+# エラー表示関数（メッセージとトークンを受け取って，そのトークンの位置に値を挿入する)
+proc errorAt*(errorMsg: string, tok: Token) = 
+  var tmp: string
+  for i in input:
+    tmp.add($i)
+  echo tmp
+  if tok == nil:
+    echo " ".repeat(idx) & "^"
+  else:
+    echo " ".repeat(tok.at) & "^"
+  quit(errorMsg)
+
+#todo ------------------------------------------------------------------------------------
 
 # ノードの種類（AST）
 type
   NodeKind* = enum
-    NdAdd,  # +
-    NdSub,  # -
-    NdMul,  # *
-    NdDiv,  # /
-    NdNum,  # 整数
-    NdEq,   # ==
-    NdNe,   # !=
-    NdL,    # <
-    NdLe,   # <=
-    NdAssign, # = 代入式
-    NdLvar,   # 変数
-    NdReturn,
-    NdExprStmt,
-    NdIf,
-    NdWhile,
-    NdFor,
-    NdBlock,
-    NdFuncall,
-    NdAddr,
-    NdDeref
+    NdAdd,              # +
+    NdSub,              # -
+    NdMul,              # \*
+    NdDiv,              # /
+    NdNum,              # 整数
+    NdEq,               # ==
+    NdNe,               # \!=
+    NdL,                # <
+    NdLe,               # <=
+    NdAssign,           # = 代入式
+    NdLvar,             # 変数
+    NdReturn,           # return
+    NdExprStmt,         # 式の文
+    NdIf,               # if
+    NdWhile,            # while
+    NdFor,              # for
+    NdBlock,            # compound statement
+    NdFuncall,          # function
+    NdAddr,             # pointer &
+    NdDeref,            # pointer *
+    NdNull              # NULL
 
 # ノード型
 type
   Node* = ref object
-    kind*: NodeKind  # ノードの種類
-    next*: Node      #! 次のノード, 複数のノードを配列(code)ではなく，連結リストで管理
-    lhs*: Node       # 左辺
-    rhs*: Node       # 右辺
-    val*: int        # kindがNdNumの場合の数値
-    arg*: Lvar       # kindがNdLvarの時
+    kind*: NodeKind     # ノードの種類
+    next*: Node         # 次のノード(連結リストで管理)
+    lhs*: Node          # 左辺
+    rhs*: Node          # 右辺
+    val*: int           # kindがNdNumの場合の数値
+    lvar*: Lvar         # kindがNdLvarの時
 
     # kindがNdIf,NdWhileの時
     cond*: Node
     then*: Node
     els*: Node
+
     # kindがNdForの時
     init*: Node
     inc*: Node
@@ -136,6 +137,8 @@ type
 
     # 型情報
     ty*: Type
+
+#? ------------------------------------------------------------------------------------
 
 # 関数ごとに管理
 type Function* = ref object
